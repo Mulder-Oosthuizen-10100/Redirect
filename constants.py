1d0fa33dc217a419d094c0e2d6bb7f705278d591
Mulder-Oosthuizen-10100 <mulder.huizen@gmail.com>
2024-04-10 21:07:11 +0200

    Coding Standard for custom tkinter

--- constants.py
+++ constants.py
@@ -1,10 +1,30 @@
 class ViewConstantsNamespace:
+    #################### 
+    # Button Constants #
+    ####################
+
+    # Font Family
     @property
     def ButtonFontFamily(self):
         return "Currier New"
-    
+
+    # Font Size
     @property
     def ButtonFontSize(self):
         return 12
+
+    ################### 
+    # Label Constants #
+    ###################
+
+    # Font Family
+    @property
+    def LabelFontFamily(self):
+        return "Currier New"
+    
+    # Font Size
+    @property
+    def LabelFontSize(self):
+        return 40
     
 ViewConstants = ViewConstantsNamespace()
--- view.py
+++ view.py
@@ -11,19 +11,61 @@ root = customtkinter.CTk()
 root.title('Redirect 404 URLs')
 root.iconbitmap('Images/RedirectLogo.ico')
 root.geometry("800x600")
+root.grid_columnconfigure((0,1), weight=1)
 
+frame = customtkinter.CTkFrame(
+    master=root,
+    width=200, 
+    height=200
+)
+
+lbl_heading = customtkinter.CTkLabel(
+    master=root,
+    corner_radius=16,
+    text="Redirect 404 URLs",
+    font=(ViewConstants.LabelFontFamily, ViewConstants.LabelFontSize)
+)
 
 def generate_redirect_file():
     return 1
 
 btn_create_redirects = customtkinter.CTkButton(
-    root,
+    master=root,
     text="Create Redirect File",
     command= generate_redirect_file,
     font=(ViewConstants.ButtonFontFamily, ViewConstants.ButtonFontSize)
 )
 
-btn_create_redirects.pack(pady=80)
+checkbox_1 = customtkinter.CTkCheckBox(root, text="checkbox 1")
+checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
+checkbox_2 = customtkinter.CTkCheckBox(root, text="checkbox 2")
+checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+
+lbl_heading.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+# btn_create_redirects.grid(row=6, column=5, padx=20, pady=20)#.pack(pady=20)
+# frame.pack(pady=20)
 
 
 root.mainloop()
+
+
+# class App(customtkinter.CTk):
+#     def __init__(self):
+#         super().__init__()
+
+#         self.title("my app")
+#         self.geometry("400x150")
+#         self.grid_columnconfigure((0, 1), weight=1)
+
+#         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
+#         self.button.grid(row=0, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+#         self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
+#         self.checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
+#         self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
+#         self.checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        
+#     def button_callback(self):
+#         print("button pressed")
+
+# app = App()
+# app.mainloop()
